223ccb32d6178957340de4c7be1173b4b3e2ae73
jwnicholas99 <jwnicholas99@gmail.com>
2020-07-20 00:23:45 +0800

    Added raindrop

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 var tl = gsap.timeline()
 
-tl.from(".main", {clipPath: "circle(0%)", duration: 2.5, ease:'Power1.easeInOut'})
+tl.from("#raindrop", {y:"-300", duration: 0.3})
+  .to("#raindrop", {height: 0, duration: 0.2})
+  .from(".main", {clipPath: "circle(0%)", duration: 1.5, ease:'Power1.easeInOut'}, "-=0.15")
