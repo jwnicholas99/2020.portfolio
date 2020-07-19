6b2aeecb2d4b0581afa96704db9e055cc4bae3d9
jwnicholas99 <jwnicholas99@gmail.com>
2020-07-19 23:20:27 +0800

    So apparently clip-path does work on Firefox - now testing if it works on mobile

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 var tl = gsap.timeline()
 
-//tl.from(".main", {width: "0%", duration: 2.5, ease:'Power1.easeInOut'})
+tl.from(".main", {clipPath: "circle(0%)", duration: 2.5, ease:'Power1.easeInOut'})
