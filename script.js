7452da2db9f1cd45f425cf120996c7abbd21824a
jwnicholas99 <jwnicholas99@gmail.com>
2020-07-26 21:23:43 +0800

    1. Added CircularStd, DroidSansMono and FireMono fonts 2. Did landing page

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ menu_toggler[0].onclick = function () {
   top_nav[0].classList.toggle('is_open');
 }
 
+
 // Accordian
 var accordian_btns = document.getElementsByClassName("accordian_btn");
 
@@ -25,3 +26,42 @@ for (var i = 0; i < accordian_btns.length; i++){
   }
 }
 
+
+
+// Typewriter
+var char_idx = 0;
+var is_deleting = false;
+var words = ["Nicholas", "a Maker", "a Developer"]; // speed in milliseconds
+var colors = ["#ff595e", "#8ac926", "#6a4c93"];
+var word_idx = 0;
+
+function type_writer() {
+  word_idx = word_idx % words.length;
+  var cur_word = words[word_idx];
+  document.getElementById("typewriter").style.background = colors[word_idx];
+  var speed = 150;
+
+  if (is_deleting){
+    speed /= 2;
+    if(char_idx >= 0){
+      document.getElementById("typewriter").innerHTML = cur_word.slice(0, char_idx);
+      char_idx--;
+      setTimeout(type_writer, speed);
+    } else {
+      is_deleting = false;
+      word_idx++;
+      setTimeout(type_writer, 1000);
+    }
+  } else {
+    if (char_idx < cur_word.length) {
+      document.getElementById("typewriter").innerHTML += cur_word.charAt(char_idx);
+      char_idx++;
+      setTimeout(type_writer, speed);
+    } else {
+      is_deleting = true;
+      setTimeout(type_writer, 1500);
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', type_writer);
